158e43077a647f97baad9ed1775bdf2b699087cb
Karasu8810 <65478552+Karasu8810@users.noreply.github.com>
2023-09-29 22:43:49 -0300

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
 import math
 def menu():
-    print("Seja Bem-Vindo")
+    print("Seja Bem-Vindo \n O que deseja fazer?\n\n Digite 1 se - Somar 2 numeros\n Digite 2 se - Subtrair 2 numeros\n Digite 3 se - Multiplicar 2 numeros\n Digite 4 se - Dividir 2 numeros\n Digite 5 se - Potenciar 1 numero pelo 2\n Digite 6 se - Tirar a raiz de 1 numero")
+    print("\n-- Regras --\n 1- Utilize apenas valores numericos validos\n 2- Se for utilizar o pi, escreva apenas pi\n 3- Ferramente feita apenas para teste de repositorio, nao critique sua simplicidade")
+    input("> ")
